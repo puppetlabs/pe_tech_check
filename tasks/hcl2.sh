5168ba5414960f885579b0bf9bce071f7d30a25d
Stefan <stefan@puppet.com>
2018-10-31 22:21:31 +0000

    hcl2 Task completed

--- tasks/hcl2.sh
+++ tasks/hcl2.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 
-# Puppet Task Name: 
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
+# Define hostname and other variables
+nodename=$(/bin/hostname -f)
+workdir="/var/tmp/hcl_data"
+
+# Run Data Capture Task
+/opt/puppetlabs/bin/puppet-task run healthcheck_lite::datacapture --token-file=$workdir/token --nodes $nodename
+
+# Run Data Package Task
+/opt/puppetlabs/bin/puppet-task run healthcheck_lite::packagedata identifier=$PT_identifier --token-file=$workdir/token --nodes $nodename
--- tasks/packagedata.sh
+++ tasks/packagedata.sh
@@ -6,7 +6,9 @@ datestamp=$(/bin/date +%d%m%Y)
 filename="hcl_"$identifier"_"$datestamp".tar"
 
 echo "Starting packaging of data..."
-cd /var/temp
-/bin/tar -cvf $filename hcl_data/*
+
+/bin/rm -f /var/tmp/hcl_data/token
+/bin/tar -cvf /var/tmp/$filename /var/tmp/hcl_data/*
+/bin/rm -rf /var/tmp/hcl_data
 echo "Data tarball created at /var/tmp/$filename."
 echo "Please make the tarball at /var/tmp/$filename available to Puppet."
