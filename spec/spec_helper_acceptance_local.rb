450596f4b95fd006b587da13a2b66c24f8627c89
Jarret Lavallee <jarret@puppet.com>
2021-10-22 08:31:52 -0600

    (WIP) Test setting yum skip_if_unavailable
    This commit is a test to validate if setting skip_if_unavailable in the
    yum.conf will help to provide stability for intermittant repo issues.

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -8,6 +8,18 @@ include PuppetLitmus
 RSpec.configure do |c|
   c.mock_with :rspec
   c.before :suite do
+    pp = <<-PUPPETCODE
+      if $facts['os']['family'] == 'RedHat' {
+        ini_setting { "yum skip_if_unavailable":
+          ensure  => present,
+          path    => '/etc/yum.conf',
+          section => 'main',
+          setting => 'skip_if_unavailable',
+          value   => 'True',
+        }
+      }
+    PUPPETCODE
+    PuppetLitmus::PuppetHelpers.apply_manifest(pp)
     # Download the plugins to ensure up-to-date facts
     PuppetLitmus::PuppetHelpers.run_shell('/opt/puppetlabs/bin/puppet plugin download')
   end
